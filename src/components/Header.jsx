c8a39cec9f3635088222b17be1f004ed41cbedb7
Maksym <makkie.makowski@gmail.com>
2024-10-13 16:38:50 -0400

    add header icon total items indicator

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,14 +4,20 @@ import { Container } from './styles/Container.styled'
 import { Flex } from './styles/Flex.styled'
 import { StyledHeader } from './styles/Header.styled'
 import { Link } from 'react-router-dom'
+import { useCart } from '../CartContext';
 
 export default function Header() {
+  const { cartItems } = useCart();
+  const totalItems = cartItems.reduce((sum,item) => sum + item.quantity, 0);
   return (
     <StyledHeader>
         <Container>
             <Flex style={{ justifyContent: 'space-between'}}>
                 <h1>TOP Desserts Shop</h1>
-                <Link to='/cart'><FaCartShopping size={24} style={{ color: 'hsl(14, 86%, 42%)' }} /></Link>
+                <Link to='/cart'>
+                  <FaCartShopping size={28} style={{ color: 'hsl(14, 86%, 42%)' }} />
+                  <span>{totalItems}</span>
+                </Link>
             </Flex>
         </Container>
     </StyledHeader>
--- src/components/styles/Header.styled.jsx
+++ src/components/styles/Header.styled.jsx
@@ -7,5 +7,17 @@ export const StyledHeader = styled.header`
     a {
         display: flex;
         align-items: center;
+        flex-direction: column-reverse;
+        text-decoration: none;
+        position: relative;
+
+        span {
+            position: absolute;
+            top: -15px;
+            font-weight: 500;
+            font-size: 0.9em;
+            color: hsl(14, 86%, 42%);
+
+        }
     }
 `
